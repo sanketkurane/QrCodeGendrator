dd72c576d6ac71c8ce44f0ad3c2e6b9604fbe4e6
sanketkurane <56022792+sanketkurane@users.noreply.github.com>
2025-02-09 17:22:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Qrcodegendrator02', // Important: Add your repo name here
+  base: 'Qrcodegendrator', // Important: Add your repo name here
   plugins: [react()],
 })
