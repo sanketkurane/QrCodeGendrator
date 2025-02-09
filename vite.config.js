b637e39cbd800d5a4fa953e611ea9ecb8bd71bcf
sanketkurane <sanketkurane5864@gmail.com>
2025-02-09 15:00:53 +0530

    Resolve merge conflicts from origin/main

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'Qrcodegendrator02', // Important: Add your repo name here
   plugins: [react()],
 })
